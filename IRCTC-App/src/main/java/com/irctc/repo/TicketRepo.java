0178cc5721d5434b4136eb9f242fcc4b45bacb38
Md Attaullah <57873149+mdattaullah@users.noreply.github.com>
2023-09-27 00:58:49 +0530

    Delete IRCTC-App directory

--- IRCTC-App/src/main/java/com/irctc/repo/TicketRepo.java
+++
@@ -1,9 +0,0 @@
-package com.irctc.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.irctc.model.Ticket;
-
-public interface TicketRepo  extends JpaRepository<Ticket, Integer>{
-
-}
