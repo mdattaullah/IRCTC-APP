0d88865bd001ea1250963680ba4da52bd85a6151
Md Attaullah <57873149+mdattaullah@users.noreply.github.com>
2023-09-27 01:12:52 +0530

    Add files via upload

---
+++ IRCTC-App/src/main/java/com/irctc/controller/TicketController.java
@@ -0,0 +1,45 @@
+package com.irctc.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import com.irctc.model.Ticket;
+import com.irctc.request.Passenger;
+import com.irctc.service.TicketService;
+
+@RestController
+public class TicketController {
+	
+	
+	@Autowired
+	private TicketService ticketService;
+	
+	@PostMapping( value="/ticket", consumes="application/json",produces="application/json")
+	public ResponseEntity<Ticket> bookTicket(@RequestBody Passenger passenger){
+		Ticket ticket= ticketService.bookTicket(passenger);
+		return new ResponseEntity<> (ticket, HttpStatus.CREATED);
+		
+	}
+	
+	
+	@GetMapping(value="/ticket/{ticketNo}",produces="application/json")
+	public ResponseEntity<Ticket> getTicket(@PathVariable ("ticketNo")  Integer ticketNo){
+		Ticket ticket=ticketService.getTicket(ticketNo);
+		return new ResponseEntity<> (ticket, HttpStatus.OK);
+		
+	}
+	@GetMapping(value="/tickets",produces="application/json")
+	public ResponseEntity<List<Ticket>> getTicket(){
+		List<Ticket> allTickets=ticketService.getAllTickets();
+		return new ResponseEntity<> (allTickets, HttpStatus.OK);
+	}
+	
+	
+}
